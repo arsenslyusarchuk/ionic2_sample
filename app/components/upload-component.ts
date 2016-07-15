9485a0a238e3d3e0912bc398278e0a67753c94dc
Arsen Slyusarchuk <arsen.slyusarchuk@perfectial.com>
2016-07-15 16:54:04 +0300

    Added save to a file

--- app/components/upload-component.ts
+++ app/components/upload-component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, Output, EventEmitter} from '@angular/core';
-import { NavController, ActionSheet} from 'ionic-angular';
+import { NavController, ActionSheet, Platform, Alert} from 'ionic-angular';
 import { Camera } from 'ionic-native';
+import { Transfer, File } from 'ionic-native';
 
 @Component({
   selector: 'upload-photo',
-  templateUrl: 'build/components/upload-component.html',
+  templateUrl: 'build/components/upload-component.html'
 })
 export class UploadComponent{
 
@@ -15,23 +16,89 @@ export class UploadComponent{
 
   // @Output() base64Image: string;
 
-  constructor( private _navController: NavController) {}
+  constructor( private platform: Platform, private _navController: NavController) {}
 
   takePicture(){
     console.log('camera will open');
     Camera.getPicture({
-        destinationType: Camera.DestinationType.DATA_URL,
+        destinationType: Camera.DestinationType.FILE_URI,
         targetWidth: 200,
         targetHeight: 200
     }).then((im) => {
       // imageData is a base64 encoded string
-      let base64Image = "data:image/jpeg;base64," + im;
-
-      this.imageCaptured.emit({
-         value: base64Image
-      })
+      // let base64Image = "data:image/jpeg;base64," + im;
+      this.saveImageToFileSystem(im);
+      // this.imageCaptured.emit({
+      //    value: base64Image
+      // })
     }, (err) => {
         console.log(err);
+    });
+  }
+
+  createDirIfNeeded(dirName) {
+    let targetPath;
+    if (this.platform.is('ios')) {
+      targetPath = cordova.file.documentsDirectory;
+    }
+    else if(this.platform.is('android')) {
+      targetPath = cordova.file.dataDirectory;
+    }
+    File.checkDir(targetPath, dirName)
+      .then((data: any) => {
+        console.log('Hooray', data);
+      })
+      .catch((error: any) => {
+        File.createDir(targetPath, dirName, true);
+        // console.log('Error', error);
+      })
+  }
+
+  saveImageToFileSystem(imageUrl){
+    this.platform.ready().then(() => {
+      let fileTransfer = new Transfer();
+      let destDir = 'filesToUpload';
+
+      let imageLocation = `${imageUrl}`;
+
+      let targetPath; // storage location depends on device type.
+
+      // make sure this is on a device, not an emulation (e.g. chrome tools device mode)
+      if(!this.platform.is('cordova')) {
+        return false;
+      }
+
+      this.createDirIfNeeded(destDir);
+
+      if (this.platform.is('ios')) {
+        targetPath = cordova.file.documentsDirectory + `${destDir}/${new Date().getTime()}.jpg`;
+      }
+      else if(this.platform.is('android')) {
+        targetPath = cordova.file.dataDirectory + `${destDir}/${new Date().getTime()}.jpg`;
+      }
+      else {
+        // do nothing, but you could add further types here e.g. windows/blackberry
+        return false;
+      }
+
+      fileTransfer.download(imageLocation, targetPath)
+        .then((result: any) => {
+            let alertSuccess = Alert.create({
+              title: 'Download Succeeded!',
+              subTitle: `${imageLocation} was successfully downloaded to: ${targetPath}`,
+              buttons: ['Ok']
+            });
+
+            this._navController.present(alertSuccess);
+        }).catch((error: any) => {
+            let alertFailure = Alert.create({
+              title: 'Download Failed!',
+              subTitle: `${imageLocation} was not successfully downloaded, please try again later`,
+              buttons: ['Ok']
+            });
+
+            this._navController.present(alertFailure);
+        });
     });
   }
 
--- app/pages/listing/listing.ts
+++ app/pages/listing/listing.ts
@@ -3,6 +3,7 @@ import { NavController, Loading, ActionSheet, Alert} from 'ionic-angular';
 import { ItemService } from '../../services/item-service';
 import { Item } from '../../models/item';
 import { UploadComponent } from '../../components/upload-component';
+import { Auth } from '../../services/auth';
 
 @Component({
   templateUrl: 'build/pages/listing/listing.html',
@@ -15,7 +16,8 @@ export class ListingPage implements OnInit  {
 
   constructor(
     private _navController: NavController,
-    private _itemService: ItemService
+    private _itemService: ItemService,
+    private _auth: Auth
    )  { this.loadingContent = true; }
 
   ngOnInit() {
@@ -29,8 +31,10 @@ export class ListingPage implements OnInit  {
         this.loadingContent = false;
         loading.dismiss();
         this.items = data
+        console.log('current User is', this._auth.currentUser);
       });
   }
+
   addItem(data){
     console.log('image data passed from component');
     console.log(data);
